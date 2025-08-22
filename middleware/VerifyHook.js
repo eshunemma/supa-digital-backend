4ddae6ab5d61f67221ae23ec44ba70f5196bffa1
Emmanuel Eshun <91864345+eshunemma@users.noreply.github.com>
2025-08-22 06:32:43 +0000

    Update VerifyHook.js

--- middleware/VerifyHook.js
+++ middleware/VerifyHook.js
@@ -1,9 +1,7 @@
 const crypto = require("crypto");
 
 function verifyWebhook(req, res, next) {
-  const secret = "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";
-  //   const secret = "5d8efd0d907bd6c5a2de2fb1e6a37f2c";
-  //   const secret = "5d8efd0d907bd6c5a2de2fb1e6a37f2c";
+  const secret = process.env.secret;
   const hmac = req.headers["x-shopify-hmac-sha256"];
   const body = JSON.stringify(req.body);
 
